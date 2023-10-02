c4eb47bf03e999eb41f0c09ee6c0c0e3d5abf620
Fr3sc0 <blass1722@gmail.com>
2023-10-02 11:33:17 -0300

    Update Conexion.java

--- src/AccesoDatos/Conexion.java
+++ src/AccesoDatos/Conexion.java
@@ -1,7 +1,6 @@
-
 package AccesoDatos;
 
-    import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
@@ -11,24 +10,24 @@ public class Conexion {
     private static final String URL = "jdbc:mariadb://localhost/";
     private static final String DB = "granhotel";
     private static final String USUARIO = "root";
-    private static final String PASSWORD= "";
+    private static final String PASSWORD = "";
     private static Connection connection;
-    
-    private Conexion (){}
-    
 
-    public static Connection getConexion(){
-        if (connection==null) {
-            
+    private Conexion() {
+    }
+
+    public static Connection getConexion() {
+        if (connection == null) {
+
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
-                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
                 JOptionPane.showMessageDialog(null, "Conectado");
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error al cargar los drivers");
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, "Error al conetar a la DB");
-            }            
+            }
         }
         return connection;
     }
