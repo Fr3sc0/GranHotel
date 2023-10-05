f0f8366a431be266a126716a3f94ae1e9b15034a
MaoZ3 <117559028+MaoZ3@users.noreply.github.com>
2023-10-05 11:22:41 -0300

    TipoHabitacion
    metodos finish

--- src/AccesoDatos/TipoHabitacionData.java
+++ src/AccesoDatos/TipoHabitacionData.java
@@ -1,7 +1,42 @@
 
 package AccesoDatos;
 
+import Entidades.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 public class TipoHabitacionData {
+           
+
+    private Connection con = null;
+
+    public TipoHabitacionData() {
+        con = Conexion.getConexion();
+    }
+    
     
 }
+
+public void agregarTipoabitacion 
+        String sql = "INSERT INTO inscripcion(idAlumno, idMateria, nota) VALUES (?,?,?)";
+        try {
+            PreparedStatement ps= con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, insc.getAlumno().getIdAlumno());
+            ps.setInt(2, insc.getMateria().getIdMateria());
+            ps.setDouble(3, insc.getNota());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                insc.setIdInscripcion(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Inscripcion registrada.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion.");
+        }
